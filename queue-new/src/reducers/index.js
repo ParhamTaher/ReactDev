d1b5b7c20db5779eaf0ec84baecab9614b4d5e04
Parham Taher <parham.taher@gmail.com>
2017-07-17 17:27:54 -0400

    Improved login and signup UI and also added validation using reduxForm

--- queue-new/src/reducers/index.js
+++ queue-new/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import QueueReducer from './QueueReducer';
 // Keep track of our current location, in the store
 import { routerReducer } from 'react-router-redux';
+import { reducer as FormReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
+    form: FormReducer,
     patientList: QueueReducer,
     router: routerReducer
 });
